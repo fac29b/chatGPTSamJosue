eb74f395e29b16ae73bb4b714df75ee8fb26905f
sam hockaday <samuel.hockaday@gmail.com>
2023-11-18 14:08:36 +0000

    Send API Successful

--- index.js
+++ index.js
@@ -5,15 +5,52 @@ const submitBtn = document.querySelector('button');
 
 submitBtn.addEventListener('click', captureFormValue)
 
+// function captureFormValue(e) {
+//     e.preventDefault();
+
+//     let storeApi = apikey.value;
+//     let textValue = textarea.value; 
+
+//     apikey.value = '';
+//     textarea.value = ''; 
+
+//     const data = { storeApi, textValue } 
+//     console.log(data);
+//     fetch("https://api.openai.com/v1/chat/completions", {
+//         method: "POST",
+//         body: JSON.stringify(data),
+//         headers: {"content-type": "application/json"},
+//     })
+// }
+
 function captureFormValue(e) {
     e.preventDefault();
 
     let storeApi = apikey.value;
-    let textValue = textarea.value; // Capture the value in a separate variable
+    let textValue = textarea.value; 
 
     apikey.value = '';
-    textarea.value = ''; // Clear the textarea
+    textarea.value = ''; 
+    
 
-    const data = { storeApi, textValue } // Use textValue here
+    const data = { messages: [{ role: 'user', content: textValue}], model: "gpt-3.5-turbo" }; // Assuming textValue is the prompt for the OpenAI API
     console.log(data);
+    fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "content-Type": "application/json",
+            "Authorization": `Bearer ${storeApi}` // Include the API key here
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Failed:', data);
+        // Process the response data as needed
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
+
+
